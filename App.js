31fb60390a2573846d03171a3034b26212055276
jehadkhalil7 <jehadkhalil910@gmail.com>
2024-08-18 20:34:01 +0300

    update

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 const express = require("express");
 var cors = require("cors");
 const mongoose = require("mongoose");
-const USER_MODEL = require("./API/Models/user.model");
-//  const Routs = require("./api/routes/Router");
+const Routs = require("./API/Routes/Routes");
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -16,27 +15,6 @@ mongoose.connection.on("connected", () => {
     console.log("mongo connected");
 });
 
+app.use(Routs)
 
-app.get("/app", (req, res) => {
-    res.status(200).json({
-        name: "Jehad",
-        city: "Nazzareth",
-    });
-});
-
-
-app.post("/watMyName", (req, res) => {
-    const { name, lastName } = req.body;
-
-    if (!name || !lastName) {
-        res.status(703).json({
-            error: true,
-            errorMessage: "name and last name are MUST!",
-        });
-        return;
-    }
-    res.status(200).json({
-        fullName: name + " " + lastName,
-    });
-});
 module.exports = app
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
-const app= require("./App");
+const app = require("./App");
 
-const port = process.env.PORT | 8000;
+const port = process.env.PORT || 8000;
 const server = http.createServer(app);
 
 app.listen(port)
